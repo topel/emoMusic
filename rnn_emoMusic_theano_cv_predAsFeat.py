7fae59275aa0ae2d51776a0de46dda6e4b7c8e05
Thomas Pellegrini <thomas.pellegrini@gmail.com>
2015-08-26 20:36:42 +0200

    output submissions

--- rnn_emoMusic_theano_cv_predAsFeat.py
+++ rnn_emoMusic_theano_cv_predAsFeat.py
@@ -23,6 +23,52 @@ logger = logging.getLogger(__name__)
 mode = theano.Mode(linker='cvm')
 #mode = 'DEBUG_MODE'
 
+def add_prediction_features(folds, train_pred_folds, test_pred_folds, feature_indices_list):
+    new_folds = list()
+    for fold_id in range(10):
+        # fold_id = 0
+        fold = folds[fold_id]
+        X_train, y_train, id_train = load_X_from_fold_to_3dtensor(fold, 'train', NUM_OUTPUT)
+        X_test, y_test, id_test = load_X_from_fold_to_3dtensor(fold, 'test', NUM_OUTPUT)
+
+        train_pred_fold = train_pred_folds[fold_id]
+        test_pred_fold = test_pred_folds[fold_id]
+
+        for seg in feature_indices_list:
+            deb = seg[0]
+            fin = seg[1]
+            X_train = np.concatenate((X_train, train_pred_fold[:,:,deb:fin]), axis=2)
+            X_test = np.concatenate((X_test, test_pred_fold[:,:,deb:fin]), axis=2)
+
+        print X_train.shape
+        data = dict()
+        data['train'] = dict()
+        data['train']['X'] = X_train
+        data['train']['y'] = y_train
+        data['train']['song_id'] = id_train
+        data['test'] = dict()
+        data['test']['X'] = X_test
+        data['test']['y'] = y_test
+        data['test']['song_id'] = id_test
+
+        new_folds.append(data)
+    return new_folds
+
+
+def load_prediction_folds(data_dir):
+    train_folds = list()
+    test_folds = list()
+    for fold_id in range(10):
+        print '... loading train FOLD %d'%fold_id
+        fold = pickle.load( open( data_dir + '/fold%d_train_predictions.pkl'%(fold_id), "rb" ) )
+        train_folds.append(fold)
+
+        print '... loading test FOLD %d'%fold_id
+        fold = pickle.load( open( data_dir + '/fold%d_test_predictions.pkl'%(fold_id), "rb" ) )
+        test_folds.append(fold)
+
+    return train_folds, test_folds
+
 def load_folds(data_dir, useEssentia=False):
     folds = list()
     for fold_id in range(10):
@@ -167,63 +213,6 @@ def rnn_cv( folds, n_hidden=10, n_epochs=50, lr=0.001, lrd = 0.999, reg_coef= 0.
 
         print X_train.shape, y_train.shape, X_test.shape, y_test.shape
 
-        if useMelodyFeatures:
-            # first feature = slope, other two = mean, std
-            melody_train, melody_test = subset_features(all_song_melody_features, id_train, id_test)
-            # melody_train = melody_train[:,:,1:]
-            # melody_test = melody_test[:,:,1:]
-
-            # standardize train data
-            melody_concat_train = np.reshape(melody_train, (melody_train.shape[0]*melody_train.shape[1], melody_train.shape[2]), order='C')
-            melody_concat_train_normed, scaler = standardize(melody_concat_train)
-            # print concat_train_normed.shape
-            melody_train_normed = np.reshape(melody_concat_train_normed, (melody_train.shape[0], melody_train.shape[1], melody_train.shape[2]), order='C')
-            del melody_concat_train, melody_concat_train_normed
-
-            # standardize test data
-            melody_concat_test = np.reshape(melody_test, (melody_test.shape[0]*melody_test.shape[1], melody_test.shape[2]), order='C')
-            melody_concat_test_normed, _ = standardize(melody_concat_test, scaler)
-            # print concat_test_normed.shape
-            melody_test_normed = np.reshape(melody_concat_test_normed, (melody_test.shape[0], melody_test.shape[1], melody_test.shape[2]), order='C')
-            del melody_concat_test, melody_concat_test_normed
-
-            # concat with the other features
-            X_train = np.concatenate((X_train, melody_train_normed), axis=2)
-            X_test = np.concatenate((X_test, melody_test_normed), axis=2)
-
-        if useTempoFeatures:
-            tempo_train, tempo_test = subset_features(all_song_tempo_features, id_train, id_test)
-            # standardize train data
-            tempo_concat_train = np.reshape(tempo_train, (tempo_train.shape[0]*tempo_train.shape[1], tempo_train.shape[2]), order='C')
-            tempo_concat_train_normed, scaler = standardize(tempo_concat_train)
-            # print concat_train_normed.shape
-            tempo_train_normed = np.reshape(tempo_concat_train_normed, (tempo_train.shape[0], tempo_train.shape[1], tempo_train.shape[2]), order='C')
-            del tempo_concat_train, tempo_concat_train_normed
-
-            # standardize test data
-            tempo_concat_test = np.reshape(tempo_test, (tempo_test.shape[0]*tempo_test.shape[1], tempo_test.shape[2]), order='C')
-            tempo_concat_test_normed, _ = standardize(tempo_concat_test, scaler)
-            # print concat_test_normed.shape
-            tempo_test_normed = np.reshape(tempo_concat_test_normed, (tempo_test.shape[0], tempo_test.shape[1], tempo_test.shape[2]), order='C')
-            del tempo_concat_test, tempo_concat_test_normed
-
-            # concat with the other features
-            X_train = np.concatenate((X_train, tempo_train_normed), axis=2)
-            X_test = np.concatenate((X_test, tempo_test_normed), axis=2)
-
-        # print id_test.shape
-
-        # X_train = X_train[0:100,:,:]
-        # y_train = y_train[0:100,:,:]
-
-        # X_train = X_train[:,[10,12,13,17,19,82,83,84,85,89,90,91,103,140,142,146,148,212,214,218,220]]
-        # X_test = X_test[:,[10,12,13,17,19,82,83,84,85,89,90,91,103,140,142,146,148,212,214,218,220]]
-        # X_train = X_train[:,[13,85,103,142,214]]
-        # X_test = X_test[:,[13,85,103,142,214]]
-
-        # X_test = X_train[119:119+y_test.shape[0],:]
-        # y_test = y_train[119:119+y_test.shape[0]]
-
 
         print X_train.shape, y_train.shape, X_test.shape, y_test.shape
         nb_seq_train, nb_frames_train, nb_features_train = X_train.shape
@@ -261,7 +250,6 @@ def rnn_cv( folds, n_hidden=10, n_epochs=50, lr=0.001, lrd = 0.999, reg_coef= 0.
             pred.append(model.predict(X_test[ind_seq_test]))
 
         y_hat = np.array(pred, dtype=float)
-        print y_hat.shape
 
         if doSmoothing:
             # smooooooth
@@ -274,24 +262,9 @@ def rnn_cv( folds, n_hidden=10, n_epochs=50, lr=0.001, lrd = 0.999, reg_coef= 0.
                 y_hat_smooth[i, :delay, 1] = y_hat[i, :delay, 1]
                 y_hat_smooth[i, -delay:, 1] = y_hat[i, -delay:, 1]
 
-
-        # save predictions on the test subset, before reshaping to 2-d arrays (I need 3d arrays)
-        if doSmoothing:
-            # fold_pred = [item for sublist in fold_pred for item in sublist]
-            # fold_pred = np.array(fold_pred, dtype=float)
-            pred_file = LOGDIR + 'fold%d_test_predictions.pkl'%(fold_id)
-            pickle.dump( y_hat_smooth, open( pred_file, "wb" ) )
-            print ' ... predictions y_hat_smooth saved in: %s'%(pred_file)
-        else:
-            # fold_pred = [item for sublist in fold_pred for item in sublist]
-            # fold_pred = np.array(fold_pred, dtype=float)
-            pred_file = LOGDIR + 'fold%d_test_predictions.pkl'%(fold_id)
-            pickle.dump( y_hat, open( pred_file, "wb" ) )
-            print ' ... predictions y_hat saved in: %s'%(pred_file)
-
-
-        if doSmoothing:
             y_hat_smooth = np.reshape(y_hat_smooth, (y_hat_smooth.shape[0]*y_hat_smooth.shape[1], y_hat_smooth.shape[2]))
+
+
         y_hat = np.reshape(y_hat, (y_hat.shape[0]*y_hat.shape[1], y_hat.shape[2]))
         y_test_concat = np.reshape(y_test, (y_test.shape[0]*y_test.shape[1], y_test.shape[2]))
 
@@ -319,43 +292,6 @@ def rnn_cv( folds, n_hidden=10, n_epochs=50, lr=0.001, lrd = 0.999, reg_coef= 0.
         )
         print s
         log_f.write(s)
-
-
-
-        # predict on the train set and save predictions (useful to train rnn2)
-        if doSmoothing:
-            pred = list()
-            for ind_seq_train in xrange(nb_seq_train):
-                pred.append(model.predict(X_train[ind_seq_train]))
-
-            train_y_hat = np.array(pred, dtype=float)
-            print train_y_hat.shape
-
-            train_y_hat_smooth = np.zeros_like(train_y_hat, dtype=float)
-            for i in xrange(train_y_hat.shape[0]):
-                train_y_hat_smooth[i, :, 0] = np.convolve(train_y_hat[i, :, 0], wts, mode='same')
-                train_y_hat_smooth[i, :delay, 0] = train_y_hat[i, :delay, 0]
-                train_y_hat_smooth[i, -delay:, 0] = train_y_hat[i, -delay:, 0]
-                train_y_hat_smooth[i, :, 1] = np.convolve(train_y_hat[i, :, 1], wts, mode='same')
-                train_y_hat_smooth[i, :delay, 1] = train_y_hat[i, :delay, 1]
-                train_y_hat_smooth[i, -delay:, 1] = train_y_hat[i, -delay:, 1]
-
-            # no reshape, I need 3d arrays
-            # train_y_hat_smooth = np.reshape(train_y_hat_smooth, (train_y_hat_smooth.shape[0]*train_y_hat_smooth.shape[1], train_y_hat_smooth.shape[2]))
-
-            pred_file = LOGDIR + 'fold%d_train_predictions.pkl'%(fold_id)
-            pickle.dump( train_y_hat_smooth, open( pred_file, "wb" ) )
-            print ' ... predictions y_hat_smooth saved in: %s'%(pred_file)
-        else:
-            pred = list()
-            for ind_seq_train in xrange(nb_seq_train):
-                pred.append(model.predict(X_train[ind_seq_train]))
-
-            train_y_hat = np.array(pred, dtype=float)
-            pred_file = LOGDIR + 'fold%d_train_predictions.pkl'%(fold_id)
-            pickle.dump( train_y_hat, open( pred_file, "wb" ) )
-            print ' ... predictions y_hat saved in: %s'%(pred_file)
-
 
         doPlot = False
         if doPlot:
@@ -470,35 +406,9 @@ if __name__ == '__main__':
     else:
         nb_features = 260
 
-    # nb_features += 260
-
-    useMelodyFeatures = False
-    if useMelodyFeatures:
-        nom = DATADIR + '/pkl/melody_features.pkl'
-        all_song_melody_features = pickle.load( open( nom, "rb" ) )
-        nb_features += 3
-
-    useTempoFeatures = False
-    if useTempoFeatures:
-        nom = DATADIR + '/pkl/fixedtempo_features.pkl'
-        all_song_tempo_features = pickle.load( open( nom, "rb" ) )
-        nb_features += 1
 
     EMO='valence'
     # EMO='arousal'
-
-    essentia_feat_indices = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 21], [21, 61], [61, 88], [88, 89], [89, 90], [90, 93], [93, 133], [133, 134], [134, 135], [135, 138], [138, 162], [162, 163], [163, 164], [164, 167], [167, 180], [180, 186], [186, 192], [192, 193], [193, 194], [194, 195], [195, 196]]
-
-    baseline_feat_indices = [[0, 2], [2, 4], [4, 6], [6, 8], [8, 10], [10, 12], [12, 14], [14, 16],
-    [16, 18], [18, 20], [20, 72], [72, 74], [74, 76], [76, 78], [78, 80], [80, 82], [82, 84],
-    [84, 86], [86, 88], [88, 90], [90, 92], [92, 94], [94, 96], [96, 98], [98, 100], [100, 102],
-    [102, 130],
-    [130, 132], [132, 134], [134, 136], [136, 138], [138, 140], [140, 142], [142, 144], [144, 146], [146, 148], [148, 150],
-    [150, 202],
-    [202, 204], [204, 206], [206, 208], [208, 210], [210, 212], [212, 214],
-    [214, 216], [216, 218], [218, 220], [220, 222], [222, 224], [224, 226], [226, 228], [228, 230], [230, 232],
-    [232, 260]]
-
 
     # NN params
     n_hidden = 10
@@ -508,54 +418,52 @@ if __name__ == '__main__':
     reg_coef = 0.01
 
     # output smoothing with a mean filter
-    doSmoothing = True
+    doSmoothing = False
 
     print '... useEssentia: %s, useSmoothing: %s'%(useEssentia, doSmoothing)
 
     # load baseline folds
     folds = load_folds(DATADIR)
 
-    if useEssentia:
-        essentia_folds = load_folds(DATADIR, useEssentia)
-        ajout_log_file_name = 'ajout_features_flatness_spectralValleys.log'
-        ajout_log_file = open(ajout_log_file_name, 'a')
+    # test using predictions as features:
+    ajout_log_file_name = 'ajout_predictions_as_features_after_smoothing.log'
+    ajout_log_file = open(ajout_log_file_name, 'w')
 
-        # for ajout in range(len(essentia_feat_indices)):
+    feature_indices_list = [[0, 2]]
+    train_pred_folds, test_pred_folds = load_prediction_folds('rnn/nfeat268_nh10_ne50_lr0.001_reg0.01_smoothed/')
 
-        feature_indices_list = list()
-        feature_indices_list.append([89, 90])
-        feature_indices_list.append([134, 135])
-        feature_indices_list.append([186, 192])
-        new_folds = add_essentia_features(folds, essentia_folds, feature_indices_list)
-        rmse, pcorr, pvalues = rnn_cv(new_folds, n_hidden, n_epochs, lr, lrd, reg_coef, doSmoothing)
-        s = (
-        'allfolds valence: %.4f %.4f arousal: %.4f %.4f deb:%d, fin:%d\n'
-          % (rmse[0], pcorr[0][0], rmse[1], pcorr[1][0], feature_indices_list[0][0], feature_indices_list[0][1])
-        )
-        print s
-        ajout_log_file.write(s)
-        ajout_log_file.close()
+    new_folds = list()
+    for fold_id in range(10):
+        # fold_id = 0
+        fold = folds[fold_id]
+        _, y_train, id_train = load_X_from_fold_to_3dtensor(fold, 'train', NUM_OUTPUT)
+        _, y_test, id_test = load_X_from_fold_to_3dtensor(fold, 'test', NUM_OUTPUT)
 
+        X_train = train_pred_folds[fold_id]
+        X_test = test_pred_folds[fold_id]
 
-    else:
-        rmse, pcorr, pvalue = rnn_cv(folds, n_hidden, n_epochs, lr, lrd, reg_coef, doSmoothing)
+        data = dict()
+        data['train'] = dict()
+        data['train']['X'] = X_train
+        data['train']['y'] = y_train
+        data['train']['song_id'] = id_train
+        data['test'] = dict()
+        data['test']['X'] = X_test
+        data['test']['y'] = y_test
+        data['test']['song_id'] = id_test
 
-        # retrait_log_file_name = 'retrait_baseline_features.log'
-        # retrait_log_file = open(retrait_log_file_name, 'w')
-        #
-        # # feature_indices_list = [[14, 16], [76, 132], [202, 232]]
-        # feature_indices_list = [[84, 130]]
-        # # for retrait in range(len(baseline_feat_indices)):
-        # #     feature_indices_list = list()
-        # #     feature_indices_list.append(baseline_feat_indices[retrait])
-        # new_folds = remove_features(folds, feature_indices_list)
-        # rmse, pcorr, pvalues = rnn_cv(new_folds, n_hidden, n_epochs, lr, lrd, reg_coef)
-        # s = (
-        # 'allfolds valence: %.4f %.4f arousal: %.4f %.4f deb:%d, fin:%d p_values: %.3f %.3f\n'
-        #   % (rmse[0], pcorr[0][0], rmse[1], pcorr[1][0], feature_indices_list[0][0], feature_indices_list[0][1], pvalues[0], pvalues[1])
-        # )
-        # print s
-        # retrait_log_file.write(s)
-        #
-        # retrait_log_file.close()
+        new_folds.append(data)
+
+    # new_folds = add_prediction_features(folds, train_pred_folds, test_pred_folds, feature_indices_list)
+
+    rmse, pcorr, pvalue = rnn_cv(new_folds, n_hidden, n_epochs, lr, lrd, reg_coef)
+
+    s = (
+    'ajout predictions as features: allfolds valence: %.4f %.4f arousal: %.4f %.4f deb:%d, fin:%d\n'
+      % (rmse[0], pcorr[0][0], rmse[1], pcorr[1][0], feature_indices_list[0][0], feature_indices_list[0][1])
+    )
+    print s
+    ajout_log_file.write(s)
+    ajout_log_file.close()
+
 
